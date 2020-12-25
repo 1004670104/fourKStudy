e2fc710f1ea66e320d74f38e62f4b77eb8692853
laijiayong <laijiayong@szcomtop.com>
2020-12-25 14:28:16 +0800

    ConcurrentHashMap测试

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -1,2 +1,10 @@
+import java.util.concurrent.ConcurrentHashMap;
+
 public class test {
+
+    public static void main(String[] args) {
+        ConcurrentHashMap concurrentHashMap = new ConcurrentHashMap();
+        concurrentHashMap.put("key", "key");
+        concurrentHashMap.get("key");
+    }
 }
